113e11c33daebb65864d2c9ba4793b751e24655a
Amine LAMINI <amine_lamini@ext.carrefour.com>
2024-12-23 02:11:54 +0100

    add dtos attributes

--- src/main/java/com/example/tree/entities/dtos/FarmDto.java
+++ src/main/java/com/example/tree/entities/dtos/FarmDto.java
@@ -1,10 +1,17 @@
 package com.example.tree.entities.dtos;
+
+import com.example.tree.entities.Client;
+import com.example.tree.entities.Tree;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class FarmDto {
+
+public record FarmDto(String clientId, String name,
+                      double latitude,
+                      double longitude,
+                      Client client,
+                      List<Tree> trees) {
+
 }
--- src/main/java/com/example/tree/entities/dtos/TreeDto.java
+++ src/main/java/com/example/tree/entities/dtos/TreeDto.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class TreeDto {
 }
